4973469f4c50f946f2b54e31e76f8309c12cf46f
Vlad Poberezhny <vlad.pbr@gmail.com>
2020-10-03 12:23:13 +0300

    feat(matrix printing, custom dimensions)

--- main.cpp
+++ main.cpp
@@ -2,25 +2,26 @@
 #include <bits/stdc++.h>
 #include "tree.h"
 
-#define VALUE_RANGE 100
+#define MATRIX_X 200
+#define MATRIX_Y 50
 #define POINTS_AMOUNT 1000
 
 Tree* BuildTree(Tree *t) {
 
     // generate random points
-    bool points[VALUE_RANGE * VALUE_RANGE] = { false };
+    bool points[MATRIX_X * MATRIX_Y] = { false };
     std::fill(points, points + (POINTS_AMOUNT * sizeof(bool)), true);
-    std::random_shuffle(points, points + (VALUE_RANGE * VALUE_RANGE));
+    std::random_shuffle(points, points + (MATRIX_X * MATRIX_Y));
 
     // build tree root from first point
     int index = -1;
     while (!points[++index]);
-    t = new Tree { .point = { .x = index % VALUE_RANGE, .y = index / VALUE_RANGE }, .left = nullptr, .right = nullptr };
+    t = new Tree { .point = { .x = index % MATRIX_X, .y = index / MATRIX_X }, .left = nullptr, .right = nullptr };
 
     // insert all points
-    while (++index < VALUE_RANGE * VALUE_RANGE) {
+    while (++index < MATRIX_X * MATRIX_Y) {
         if (points[index]) {
-            t = AVL::insert(t, { .x = index % VALUE_RANGE, .y = index / VALUE_RANGE });
+            t = AVL::insert(t, { .x = index % MATRIX_X, .y = index / MATRIX_X });
         }
     }
 
@@ -49,17 +50,42 @@ Point NearestRightPoint(Tree *t, int x0) {
     return nearest;
 }
 
+void printMatrix(Tree *t, int x0) {
+    Tree *p = t;
+    bool matrix[MATRIX_Y][MATRIX_X] = { false };
+
+    // fill matrix with points
+    static void (*fill)(bool[MATRIX_Y][MATRIX_X], Tree*) = [](bool matrix[MATRIX_Y][MATRIX_X], Tree* t) {
+        if (t) {
+            matrix[t->point.y][t->point.x] = true;
+
+            fill(matrix, t->right);
+            fill(matrix, t->left);
+        }
+    };
+    fill(matrix, p);
+
+    // print matrix
+    for (int i = 0; i < MATRIX_Y; i++) {
+        for (int j = 0; j < MATRIX_X; j++) {
+            std::cout << (j == x0 ? "|" : (matrix[i][j] ? "*" : " "));
+        }
+        std::cout << "\n";
+    }
+}
+
 int main() {
     
     srand(time(0));
+    int x0 = 45;
 
     // build random tree
     Tree *pRoot = nullptr;
     pRoot = BuildTree(pRoot);
 
-    // find nearest right point
-    Point ans = NearestRightPoint(pRoot, 25);
-    std::cout << ans;
+    // display matrix
+    printMatrix(pRoot, x0);
 
-    return 1;
+    // find nearest right point
+    std::cout << NearestRightPoint(pRoot, x0);
 }
